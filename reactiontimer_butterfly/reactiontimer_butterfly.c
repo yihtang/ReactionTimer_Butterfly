f901bfc5a7106b2dd96a5ee5faf44356abe8bcc5
YihTang <yeoyihtang@live.com>
2012-08-22 12:11:20 +0800

    Initial commit

---
+++ reactiontimer_butterfly/reactiontimer_butterfly.c
@@ -0,0 +1,17 @@
+/*
+ * reactiontimer_butterfly.c
+ *
+ * Created: 8/22/2012 12:10:05 PM
+ *  Author: ASUS-U45JC
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
