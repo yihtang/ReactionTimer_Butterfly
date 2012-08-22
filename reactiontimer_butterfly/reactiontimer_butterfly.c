b292906daa7d2b393ad6f8ac31a8ffd9c5374551
YihTang <yeoyihtang@live.com>
2012-08-22 15:43:55 +0800

    Attemps to fix problems

--- reactiontimer_butterfly/reactiontimer_butterfly.c
+++ reactiontimer_butterfly/reactiontimer_butterfly.c
@@ -9,7 +9,7 @@
  */ 
 
 #define F_CPU 2000000
-#define MAX_COUNT 3905 // for 2 seconds 
+#define MAX_COUNT 16810 // for 4 seconds 
 
 #include <stdlib.h>
 #include <avr/io.h>
@@ -32,16 +32,16 @@ int main(void)
 	LCD_Init();
 		
 	// use PINB1 and PINB0 as input to check if pressed
-	DDRB &= ~((1<< PINB0) | (1 << PINB1));
+	DDRB &= ~((1<< PINB1) | (1 << PINB2));
 	// use PINB5 as output (Piezo element on AVR Butterfly) to indicate game start
 	DDRB |= (1<<PINB5);
 	
 	//set up PINB0 to detect if game resets (restarts), all are high (5V) initially
-	PORTB &= ~((1<< PINB0) | (1 << PINB1));
+	PORTB = 0;
 	// enable external interrupts on PCINT8-PCINT15
 	EIMSK |= (1<<PCIE1);
 	EIFR |= (1<<PCIF1);
-	PCMSK1 |= (1<<PCINT8); // individual interrupt enabler for PCINT8 (PINB0)
+	PCMSK1 |= (1<<PCINT9); // individual interrupt enabler for PCINT8 (PINB1)
 	sei();
 	
     while(1)
@@ -54,16 +54,16 @@ int main(void)
 			
 			// disable PINB1 and TNCT CTC interrupt
 			TIMSK1 &= ~(1<<OCIE1A);
-			PCMSK1 &= ~(1<<PCINT9);
+			PCMSK1 &= ~(1<<PCINT10);
 		}			
 			
-		if (game_start == 1)
+		else if (game_start == 1)
 		{
 			game_buttonpressed = 0;
 			game_lastscore = 0;
 			
 			// delay
-			_delay_ms(2000);
+			_delay_ms(10000);
 			
 			// set up to give a buzz to indicate game has started
 			PORTB |= (1<<PINB5); // on buzz, turn on light when game is active
@@ -77,22 +77,23 @@ int main(void)
 			TIMSK1 |= (1<<OCIE1A);
 			
 			// enable button interrupt for PCINT9 (PINB1)
-			PCMSK1 |= (1<<PCINT9);
+			PCMSK1 |= (1<<PCINT10);
 			
 			game_start = 2;
 		}
-		if ((game_start == 2)&&(game_buttonpressed==1)){
+		else if ((game_start == 2)&&(game_buttonpressed==1)){
 			cli();
-			game_lastscore = TCNT1/MAX_COUNT*1000;
+			game_lastscore = TCNT1/(MAX_COUNT/1000);
 			game_buttonpressed = 0; // clear button press to prevent conflict
 			//disable interrupts that are set when game_start = 1
 			TIMSK1 &= ~(1<<OCIE1A);
-			PCMSK1 &= ~(1<<PINB1);	
+			PCMSK1 &= ~(1<<PCINT10);	
+			PORTB &= ~(1<<PINB5); // turn off LED when done
 			sei();
 		}
 		
 		
-		itoa(PORTB, score, 10);		
+		itoa(PORTB, score, 16);		
 		LCD_puts(score);	
 		
     }
@@ -106,14 +107,14 @@ ISR(SIG_PIN_CHANGE1)
 	
 	
 	// if PB0 input is high (RESET is pressed)
-	if (!(PORTB & (1<<PINB0))){
+	if ((PORTB & (1<<PINB1))){
 		game_start = 1;
-		LCD_puts("PINB0 HIGH");
+		//LCD_puts("PINB0 HIGH");
 	}
 	
 	// if PB1 is high (user presses button after game started)
-	if (!(PORTB & (1<<PINB1))){
-		LCD_puts("PINB1 HIGH");
+	else if ((PORTB & (1<<PINB2))){
+		//LCD_puts("PINB1 HIGH");
 		// disable PINB1 button until game and timer have both started!
 		if (game_start == 2)
 			game_buttonpressed = 1;
@@ -135,7 +136,7 @@ ISR(TIMER1_COMPA_vect)
 	
 	//disable interrupts that are set when game_start = 1
 	TIMSK1 &= ~(1<<OCIE1A);
-	PCMSK1 &= ~(1<<PCINT9);
+	PCMSK1 &= ~(1<<PCINT10);
 		
 	sei();
 }
